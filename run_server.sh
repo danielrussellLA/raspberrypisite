1c1ee85f7328d82c986f57f9f7e646077b602797
danielrussellLA <danielrussell16@gmail.com>
2016-11-15 08:37:38 -0800

    typo

--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 PORT=80 nohup node server.js > stdout.txt 2> stderr.txt &
 echo 'server now running indefinitely on port 80'
-echo 'to kill server, type "ps -ef | grep node" and then run "kill 1234" where "1234 is the PID of the node server.js process"'
+echo 'to kill server, type "ps -ef | grep node" and then run "kill 1234" where "1234" is the PID of the node server.js process'

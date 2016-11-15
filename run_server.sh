7f0de89d2db90a358a620e9a7914e7fee44f2bca
danielrussellLA <danielrussell16@gmail.com>
2016-11-15 08:54:02 -0800

    improved run server script

--- run_server.sh
+++ run_server.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+node_serverPID=($(ps -ef | grep 'node server.js'))
+kill ${node_serverPID[1]}
+echo 'killed node server process at:'
+echo ${node_serverPID[1]}
 PORT=80 nohup node server.js > stdout.txt 2> stderr.txt &
 echo 'server now running indefinitely on port 80'
 echo 'to kill server, type "ps -ef | grep node" and then run "kill 1234" where "1234" is the PID of the node server.js process'

5aeda49f5b913638568baa26d5e90663c72db9a5
danielrussellLA <danielrussell16@gmail.com>
2016-11-15 10:09:31 -0800

    space

--- run_server.sh
+++ run_server.sh
@@ -3,5 +3,5 @@ node_serverPID=($(ps -ef | grep 'node server.js'))
 kill ${node_serverPID[1]}
 echo 'killed node server process at:'
 echo ${node_serverPID[1]}
-PORT=80 nohup node server.js > stdout.txt 2> stderr.txt &
+PORT=80 nohup node server.js > stdout.txt 2 > stderr.txt &
 echo 'server now running indefinitely on port 80'
